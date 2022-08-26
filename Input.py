7ea09698d223370607bd9a6a11a523ead04acb59
Wilhelm <jlucking1310@gmail.com>
2022-08-26 13:26:55 -0400

    Inputs and length

--- Input.py
+++ Input.py
@@ -8,4 +8,6 @@ print("It's good to meet you, " + myName +". Who is your favorite Dune character
 faveDune = input()
 print("Nice! I like " + faveDune + " too! How old are you?")
 myAge = input()
-print("You will be " + str(int(myAge) + 1) + " in a year.")
+print("You will be " + str(int(myAge) + 1) + " in a year.\nNext, you can enter your favorite quote from Dune and I will tell you how many characters are in in.")
+quoteDune = input()
+print("Your Dune quote has " + str(int(len(quoteDune))) + " characters in it.")
